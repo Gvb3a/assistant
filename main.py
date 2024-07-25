15414a56bbb6c5346f42cbe9b5a6361ec4c3178a
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-07-25 19:57:25 +0300

    mail
    Reading mail (inline)

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import InputMediaPhoto, Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from deep_translator import GoogleTranslator
+
 from dotenv import load_dotenv
+from colorama import Fore, Style, init
 
-from api import mail
+from api import mail, gmail_modify
 
-
+init()
 load_dotenv()
 bot_token = os.getenv('BOT_TOKEN')
 
@@ -15,25 +18,94 @@ bot_token = os.getenv('BOT_TOKEN')
 bot = Bot(token=bot_token)
 dp = Dispatcher()
 
+def ok(s):
+    return f'{Fore.GREEN}{s}{Style.RESET_ALL}'
+
+
+
+
+async def mail_message(chat_id):
+    text, dict_of_unread = mail()
+
+    inline_keyboard = []
+    if dict_of_unread:        
+        inline_keyboard.append([InlineKeyboardButton(text=str(index), callback_data=f'mail_modify-{index}-{message_id}') for index, message_id in dict_of_unread.items()])
+        inline_keyboard.append([InlineKeyboardButton(text='Translate', callback_data='translate')]) # callback.message.text
+
+
+    await bot.send_message(chat_id=chat_id, text=text, reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboard), parse_mode='Markdown')
+
+
+@dp.callback_query(F.data.startswith('mail_modify-'))
+async def mail_modify(callback: CallbackQuery):
+
+    text = callback.message.text
+    data = callback.data.split('-')
+    reply_markup = callback.message.reply_markup
+    index = data[1]
+    email_id = data[2]
+
+    
+    gmail_modify(email_id, account=int(index[0])-1) 
+
+
+    index_1 = text.index(index)
+    index_2 = text.index('\n', index_1+1)
+    temp = text[index_1:index_2]
+    
+    if temp[-1] in '✅❌':
+        temp = temp[:-1] + ('✅' if temp[-1]=='❌' else '❌')
+        text = text[:index_1]+temp+text[index_2:]
+        
+    else:
+        temp += '✅'
+        index_3 = text.index('\n', index_2+1)
+        index_4 = text.find('\n', index_3+1)
+        text = text[:index_1]+temp+text[index_2:index_3]+text[index_4:]
+
+    await callback.message.edit_text(text=text, reply_markup=reply_markup)
+    await callback.answer()
+    
+
+@dp.callback_query(F.data.startswith('translate'))
+async def mail_modify(callback: CallbackQuery):
+
+    text = callback.message.text
+    reply_markup = callback.message.reply_markup
+
+    translated_text = GoogleTranslator(source='en', target='ru').translate(text)
+    
+    await callback.message.edit_text(text=translated_text, reply_markup=reply_markup)
+    await callback.answer()
+
+
+@dp.message(Command('mail'))
+async def dp_message(message: Message) -> None:
+    await message.answer('Just a moment...')
+    await mail_message(message.chat.id)
+    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id+1)
+
+    print(ok('Command mail'))
+
 
 @dp.message(F.text)
 async def dp_message(message: Message) -> None:
-    message_text = message.text
 
-    if 'mail' in message_text.lower() or 'почта' in message_text.lower():
-        text, list_of_unread = mail()
-        inline_keyboard = []
-        for id, k in list_of_unread:
-            inline_keyboard.append([InlineKeyboardButton(text=str(k), callback_data=str(id))])
-        await message.answer(text, parse_mode='Markdown', reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboard))
+    chat_id = message.chat.id
+    text = message.text
+    user = message.from_user.full_name
+    username = message.from_user.username
+    message_id = message.message_id
+
+    if 'mail' in text or 'почт' in text:
+        await message.answer('Just a moment...')  # TODO: temporary response will be generated while processing is in progress
+        await mail_message(chat_id)
+        await bot.delete_message(chat_id=chat_id, message_id=message_id+1)
 
 
-@dp.callback_query(F.data)   # воссоздает то же меню, что и /start
-async def inline_back_handler(callback: CallbackQuery):
-    l = sql_mode_or_language(callback.from_user.id, 'language')
-    await callback.message.edit_text(text=start_list[l], reply_markup=start_keyboard(l))
-    await callback.answer()
+    print(ok(f'{text} by {user}({username})'))
+
 
 if __name__ == '__main__':
-    print('Bot is launched')
+    print(ok('Bot is launched'))
     dp.run_polling(bot)
