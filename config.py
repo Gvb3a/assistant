aa361df29a94b0807fc213e4b092b1c15a3b3d58
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-07-30 21:42:40 +0300

    Add LOCAL_LLM
    And moved the Promtas over here. The plan is to move the settings to the database, but also to leave the config as a collection of initial settings

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 LOCAL_WHISPER = False  # If true, whisper is used locally, otherwise via the Groq API.
+LOCAL_LLM = False
+system_promt = f"""You are a useful assistant with access to various services (google calendar, todoist), but mostly you communicate in a simple way (correspondence). 
+You communicate with Boris - he is the one who created you and the creator. Never lie"""
+guiding_promt = f"""You are the chatbot's assistant. You need to choose a number between 0 and 4. 0, if the answer does not require any sources (just chatting in the chatbot). 
+1, if the answer REQUIRES a search in a vector database (to retrieve something from memory). 2 - if the answer requires a Google calendar query (plan for the day/week).  
+3 - if the answer requires a todolist query (tasks). 4 - if the response requires an email query. 
+The response MUST ONLY consist of numbers 0-4 depending on the condition (you should not respond like a chat bot). User message: """
